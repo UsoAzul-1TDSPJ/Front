f5d07e4d0f1c56765bd0bb31d0310a38624de74a
hector <hectorsoria2@outlook.com>
2025-06-02 10:25:13 -0300

    Adicionado mais dicas, e correcao no js

--- js/dicas.js
+++ js/dicas.js
@@ -2,15 +2,16 @@ const dicas = document.querySelectorAll('.dica');
 
 dicas.forEach(dica => {
     dica.addEventListener('click', () => {
-       
+        
         const currentColor = dica.style.backgroundColor;
 
         if (currentColor === 'rgb(52, 152, 219)') { 
             dica.style.backgroundColor = 'white';
-            dica.style.color = 'black'; 
+            dica.style.color = 'black';
+        } else {
             
             dica.style.backgroundColor = '#3498db';
-            dica.style.color = 'white';
+            dica.style.color = 'white'; 
         }
     });
 });
